b11367ee43d86c263f970983afc4a9d90633e590
Zach Leone <zjleone@gmail.com>
2023-02-07 19:01:51 -0700

    added the generateQuote and displayQuote functions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,23 @@
-function getQuote() {
-    
+var requestURL = "https://api.goprogram.ai/inspiration";
+
+//Calls the API to get a random quote when the Motive Me button is pressed
+function generateQuote() {
+    event.preventDefault();
+
+    fetch(requestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            displayQuote(data);
+        })
 }
+
+
+//Displays the quote on the screen
+function displayQuote(data) {
+    var author = data.author;
+    var quote = data.quote;
+
+}
+
